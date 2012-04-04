f5894492640364fe76b86cef7c17f5c6ad7fb3be
Victor Villca Villca <victor.villca.v@gmail.com>
2012-04-04 16:53:07 -0400

    ADM, U.C. Privilege Creating model, mapper and patch

---
+++ application/models/DbTable/Privilege.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 
+ * Table abstract of tblManagerial
+ * @author Victor Villca <victor.villca@swissbytes.ch>
+ *
+ */
+
+class Model_DbTable_Privilege extends Zend_Db_Table_Abstract {
+	protected $_name = 'tblPrivilege';
+}
