045fab80258f2bc2dd59e4fd8a3ce1be52f85cfe
victor villca <victor.villca@swissbytes.ch>
2012-04-10 21:42:46 -0400

    ADM, U.C. Province: Updating and Cleaning class model and mapper

--- application/models/ProvinceMapper.php
+++ application/models/ProvinceMapper.php
@@ -1,24 +1,28 @@
 <?php
 /**
- * DIST
- * Done
+ * DataMapper for Dist 2.
+ * 
  * @category Dist
- * @package application\models
+ * @package Models
  * @author Victor Villca <victor.villca@swissbytes.ch>
  * @copyright Copyright (c) 2012 Gisof A/S
  * @license Proprietary
  */
 
-class Model_ProvinceMapper {	
+class Model_ProvinceMapper extends Model_TemporalMapper {	
 	
 	/**
 	 * 
-	 * Enter description here ...
-	 * @var unknown_type
+	 * This class abstract is from Zend framework
+	 * @var Zend_Db_Table_Abstract
 	 */
 	protected $_dbTable;
-
-    private function setDbTable($dbTable) {
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see Model_TemporalMapper::setDbTable()
+	 */
+    protected function setDbTable($dbTable) {
         if (is_string($dbTable)) {
             $dbTable = new $dbTable();
         }
@@ -29,6 +33,10 @@ class Model_ProvinceMapper {
         return $this;
     }
 	
+    /**
+     * (non-PHPdoc)
+     * @see Model_TemporalMapper::getDbTable()
+     */
     public function getDbTable() {
         if (null === $this->_dbTable) {
             $this->setDbTable('Model_DbTable_Province');
@@ -43,11 +51,11 @@ class Model_ProvinceMapper {
      */
 	public function save(Model_Province $province) {
         $data = array(
-            'name'   => $province->getName(),
+            'name' => $province->getName(),
             'description' => $province->getDescription(),
         	'departmentId' => $province->getDepartment()->getId(),
             'created' => date('Y-m-d H:i:s'),
-        	'state' => TRUE
+        	self::STATE_FIELDNAME => TRUE
         );
         
 		unset($data['id']);
@@ -67,7 +75,7 @@ class Model_ProvinceMapper {
         }
         
 	 	$data = array(
-            'name'   => $province->getName(),
+            'name' => $province->getName(),
             'description' => $province->getDescription(),
 	 		'departmentId' => $province->getDepartment()->getId(),
             'changed' => date('Y-m-d H:i:s')
@@ -89,47 +97,149 @@ class Model_ProvinceMapper {
         
         $data = array(
             'changed' => date('Y-m-d H:i:s'),
-        	'state' => FALSE,
+        	self::STATE_FIELDNAME => FALSE,
         );
         
     	$this->getDbTable()->update($data, array('id = ?' => $id));
     }
 
-    /**
-     * 
-     * Finds model
-     * @param int $id
-     * @param Model_Province $province
+	/**
+     * (non-PHPdoc)
+     * @see Model_TemporalMapper::find()
+     * @return Model_Province
      */
-    public function find($id, Model_Province $province) {
+	public function find($id) {
         $result = $this->getDbTable()->find($id);
     	if (0 == count($result)) {
-            return;
+            return NULL;
         }
         
         $row = $result->current();
         
-        $department = new Model_Department();
         $departmentMapper = new Model_DepartmentMapper();
-        $departmentMapper->find($row->departmentId, $department);
-        
-        $province->setId($row->id)
+        $department = $departmentMapper->find($row->departmentId);
+
+        $province = new Model_Province();
+        $province
         		->setName($row->name)
         		->setDescription($row->description)
         		->setDepartment($department)
         		->setCreated($row->created)
         		->setChanged($row->changed)
+        		->setId($row->id)
         		;
+    	return $province;
     }
-     
+
+    /**
+     * (non-PHPdoc)
+     * @see Model_TemporalMapper::findAll()
+     */
+	public function findAll() {
+    	$whereState = sprintf("%s = 1", self::STATE_FIELDNAME);
+        $resultSet = $this->getDbTable()->fetchAll($whereState);
+        
+        $entries = array();
+        foreach ($resultSet as $row) {
+        	$departmentMapper = new Model_DepartmentMapper();
+        	$department = $departmentMapper->find($row->departmentId);
+        	
+            $entry = new Model_Province();
+            $entry->setName($row->name)
+            	->setDescription($row->description)
+            	->setDepartment($department)
+            	->setCreated($row->created)
+            	->setChanged($row->changed)
+            	->setId($row->id)
+            	;
+            $entries[] = $entry;
+        }
+        
+        return $entries;
+    }
+    
+    /**
+     * (non-PHPdoc)
+     * @see Model_TemporalMapper::findByCriteria()
+     */
+	public function findByCriteria($filters = array(), $limit = NULL, $offset = NULL, $sortColumn = NULL, $sortDirection = NULL) {
+		
+		$where = $this->getFilterQuery($filters);
+					
+		// Order
+		$order = '';
+		switch ($sortColumn) {
+			case 1:
+				$order = 'name';
+				break;
+				
+			case 2:
+				$order = 'description';
+				break;
+			
+			case 3:
+				$order = 'departmentId';
+				break;
+				
+			case 4:
+				$order = 'created';
+				break;
+				
+			case 5:
+				$order = 'changed';
+				break;
+				
+			default: $order = 'name';
+		}
+		
+		$sortOrder = sprintf("%s %s", $order, $sortDirection);
+		
+		$whereState = sprintf("%s = 1", self::STATE_FIELDNAME);
+        $resultSet = $this->getDbTable()->fetchAll("$whereState $where", $sortOrder, $limit, $offset);
+        $entries = array();
+        foreach ($resultSet as $row) {
+        	$departmentMapper = new Model_DepartmentMapper();
+        	$department = $departmentMapper->find($row->countryId);
+        	
+            $entry = new Model_Province();
+            $entry->setName($row->name)
+            	->setDescription($row->description)
+            	->setDepartment($department)
+            	->setCreated($row->created)
+            	->setChanged($row->changed)
+            	->setId($row->id);
+            $entries[] = $entry;
+        }
+        
+        return $entries;
+    }
+    
+    /**
+     * (non-PHPdoc)
+     * @see Model_TemporalMapper::getTotalCount()
+     */
+	public function getTotalCount($filters = array()) {
+		$where = $this->getFilterQuery($filters);
+		
+		$whereState = sprintf("%s = 1", self::STATE_FIELDNAME);
+		$resultSet = $this->getDbTable()->fetchAll("$whereState $where");
+		
+		return count($resultSet);
+    }
+    
     /**
      * 
-     * Finds models
-     * @return array
+     * Verifies if the name province already exist it.
+     * @return boolean
      */
-    public function fetchAll() {
-        $entries = $this->getDbTable()->fetchAll('state = 1');
-        return $entries;
+	public function verifyExistName($name) {
+    	$whereState = sprintf("%s = 1", self::STATE_FIELDNAME);
+    	$resultSet = $this->getDbTable()->fetchRow("$whereState AND name = '$name'");
+    	if ($resultSet != NULL) {
+    		return TRUE;
+    	} else {
+    		return FALSE;
+    	}
     }
     
  	/**
@@ -137,12 +247,13 @@ class Model_ProvinceMapper {
      * Finds the names of the models
      * @return array
      */
-    public function fetchAllName() {
-    	$resultSet = $this->getDbTable()->fetchAll('state = 1');
+	public function fetchAllName() {
+		$whereState = sprintf("%s = 1", self::STATE_FIELDNAME);
+    	$resultSet = $this->getDbTable()->fetchAll($whereState);
         $data = array();
         foreach ($resultSet as $row) 
             $data[$row->id] = $row->name;
             
         return $data;
-    }	
+    }
 }
