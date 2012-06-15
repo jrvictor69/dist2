d97455b4a07d39dab19fd3e85e612e10fb9d986c
Victor Villca Villca <victor.villca.v@gmail.com>
2012-06-15 15:35:54 -0400

    Guest, U.C. Pathfinder: Adding the upload profile picture

--- application/controllers/PathfinderController.php
+++ application/controllers/PathfinderController.php
@@ -45,4 +45,21 @@ class PathfinderController extends App_Controller_Action {
 	
 	public function orionAction() {
 	}
+	
+	public function uploadAction() {
+		$this->_helper->layout()->disableLayout();
+		$this->_helper->viewRenderer->setNoRender(true);
+
+		$target_path = "image/upload/";
+		// Set the new path with the file name
+		$target_path = $target_path . basename( $_FILES['myfile']['name']); 
+		// Move the file to the upload folder
+		if(move_uploaded_file($_FILES['myfile']['tmp_name'], $target_path)) {
+			// print the new image path in the page, and this will recevie the javascripts 'response' variable
+    		echo "/".$target_path;
+		} else{
+			// Set default the image path if any error in upload.
+    		echo "default.jpg";
+		}
+	}
 }
