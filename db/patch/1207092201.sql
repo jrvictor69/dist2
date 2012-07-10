ccd2183f5019cd7fdcefb97b89a5ca864d808fb2
Victor Villca Villca <victor.villca.v@gmail.com>
2012-07-10 13:21:52 -0400

    ADM, U.C. Picture Updating the path 1207092201

--- db/patch/1207092201.sql
+++ db/patch/1207092201.sql
@@ -1,29 +1,69 @@
-CREATE  TABLE IF NOT EXISTS `tblGalleryView` (
-  `id` 				INT				NOT NULL AUTO_INCREMENT,
-  `title` 			VARCHAR(50) 	NOT NULL,
-  `description`		TEXT			NULL,
-  `filename` 		VARCHAR(150)	NOT NULL,
-  `mimeType` 		VARCHAR(45) 	NOT NULL,
-  `src`		 		VARCHAR(150)	NOT NULL,
-  `filenameCrop` 	VARCHAR(150)	DEFAULT NULL,
-  `mimeTypeCrop` 	VARCHAR(45) 	DEFAULT NULL,
-  `srcCrop`		 	VARCHAR(150)	DEFAULT NULL,
-  `type` 			INT(11) 		NOT NULL DEFAULT '1',
-  `created` 		DATETIME 		NOT NULL,
-  `changed` 		DATETIME 		DEFAULT NULL,
-  `createdBy` 		INT(11) 		DEFAULT NULL,
-  `changedBy` 		INT(11) 		DEFAULT NULL,
-  `state` 			TINYINT(1) 		NOT NULL DEFAULT '1',
-  `clubId`			INT 			DEFAULT NULL,
-  CONSTRAINT `pk_tblGalleryView`
+CREATE  TABLE IF NOT EXISTS `tblPictureType` (
+  `id` 			INT				NOT NULL AUTO_INCREMENT,
+  `name` 		VARCHAR(50) 	NOT NULL,
+  `description`	TEXT			NULL,
+  `created` 	DATETIME 		NOT NULL,
+  `changed` 	DATETIME 		DEFAULT NULL,
+  `createdBy` 	INT(11) 		DEFAULT NULL,
+  `changedBy` 	INT(11) 		DEFAULT NULL,
+  `state` 		TINYINT(1) 		NOT NULL DEFAULT '1',
+  CONSTRAINT `pk_tblChurch`
 	PRIMARY KEY (`id`) ,
 
-  KEY `i_tblGalleryView_id` (`id`),
-  INDEX `i_tblGalleryView_state_id` (`state`, `id`),
-  INDEX `fk_tblGalleryView_clubId` (`clubId`),
+  KEY `i_tblPictureType_id` (`id`) ,
+  INDEX `i_tblPictureType_state_id` (`state`, `id`) )
+ENGINE = INNODB
 
-  CONSTRAINT `fk_tblGalleryView_clubId`
-  	FOREIGN KEY (`clubId`) 
-  	REFERENCES `tblClubPathfinder` (`id`)
-  	ON UPDATE CASCADE)
+CREATE  TABLE IF NOT EXISTS `tblPictureCategory` (
+  `id` 			INT				NOT NULL AUTO_INCREMENT,
+  `name` 		VARCHAR(50) 	NOT NULL,
+  `description`	TEXT			NULL,
+  `created` 	DATETIME 		NOT NULL,
+  `changed` 	DATETIME 		DEFAULT NULL,
+  `createdBy` 	INT(11) 		DEFAULT NULL,
+  `changedBy` 	INT(11) 		DEFAULT NULL,
+  `state` 		TINYINT(1) 		NOT NULL DEFAULT '1',
+  CONSTRAINT `pk_tblChurch`
+	PRIMARY KEY (`id`) ,
+
+  KEY `i_tblPictureCategory_id` (`id`) ,
+  INDEX `i_tblPictureCategory_state_id` (`state`, `id`) )
+ENGINE = INNODB
+
+CREATE  TABLE IF NOT EXISTS `tblPicture` (
+  `id` 					INT				NOT NULL AUTO_INCREMENT,
+  `title` 				VARCHAR(50) 	NOT NULL,
+  `description`			TEXT			NULL,
+  `filename` 			VARCHAR(150)	NOT NULL,
+  `mimeType` 			VARCHAR(45) 	NOT NULL,
+  `src`		 			VARCHAR(150)	NOT NULL,
+  `filenameCrop` 		VARCHAR(150)	DEFAULT NULL,
+  `mimeTypeCrop` 		VARCHAR(45) 	DEFAULT NULL,
+  `srcCrop`		 		VARCHAR(150)	DEFAULT NULL,
+  `created` 			DATETIME 		NOT NULL,
+  `changed` 			DATETIME 		DEFAULT NULL,
+  `createdBy` 			INT(11) 		DEFAULT NULL,
+  `changedBy` 			INT(11) 		DEFAULT NULL,
+  `state` 				TINYINT(1) 		NOT NULL DEFAULT '1',
+  `clubId`				INT 			DEFAULT NULL,
+  `pictureTypeId`		INT 			DEFAULT NULL,
+  `pictureCategoryId`	INT 			DEFAULT NULL,
+  CONSTRAINT `pk_tblPicture`
+	PRIMARY KEY (`id`) ,
+
+  KEY `i_tblPicture_id` (`id`),
+  INDEX `i_tblPicture_state_id` (`state`, `id`),
+  INDEX `fk_tblPicture_clubId` (`clubId`),
+
+  CONSTRAINT `fk_tblPicture_clubId`
+	FOREIGN KEY (`clubId`)
+	REFERENCES `tblClubPathfinder` (`id`)
+	ON UPDATE CASCADE,
+	CONSTRAINT `fk_tblPicture_pictureTypeId`
+	FOREIGN KEY (`pictureTypeId`)
+	REFERENCES `tblPictureType` (`id`)ON UPDATE CASCADE,
+  CONSTRAINT `fk_tblPicture_pictureCategoryId`
+	FOREIGN KEY (`pictureCategoryId`)
+	REFERENCES `tblPictureCategory` (`id`)
+	ON UPDATE CASCADE)
 ENGINE = INNODB
