ac92ef3a237492004eb63dfdf3e5f4fd6177ccea
victor villca <victor.villca@swissbytes.ch>
2012-08-24 21:30:31 -0400

    Admin, U.C. Privilege: Removing method exception in the controller

--- application/modules/admin/controllers/PrivilegeController.php
+++ application/modules/admin/controllers/PrivilegeController.php
@@ -62,23 +62,19 @@ class Admin_PrivilegeController extends App_Controller_Action {
 
 		$formData = $this->_request->getPost();
 		if ($form->isValid($formData)) {
-			try {
-				$privilegeMapper = new Model_PrivilegeMapper();
-				if (!$privilegeMapper->verifyExistName($formData['name'])) {
-					$privilege = new Model_Privilege($formData);
-					$privilege->setCreatedBy(Zend_Auth::getInstance()->getIdentity()->id);
+			$privilegeMapper = new Model_PrivilegeMapper();
+			if (!$privilegeMapper->verifyExistName($formData['name'])) {
+				$privilege = new Model_Privilege($formData);
+				$privilege->setCreatedBy(Zend_Auth::getInstance()->getIdentity()->id);
 
-					$privilegeMapper->save($privilege);
+				$privilegeMapper->save($privilege);
 
-					$this->stdResponse->success = TRUE;
-					$this->stdResponse->message = _("Privilege saved");
-				} else {
-					$this->stdResponse->success = FALSE;
-					$this->stdResponse->name_duplicate = TRUE;
-					$this->stdResponse->message = _("The Privilege already exists");
-				}
-			} catch (Exception $e) {
-				$this->exception($this->view, $e);
+				$this->stdResponse->success = TRUE;
+				$this->stdResponse->message = _("Privilege saved");
+			} else {
+				$this->stdResponse->success = FALSE;
+				$this->stdResponse->name_duplicate = TRUE;
+				$this->stdResponse->message = _("The Privilege already exists");
 			}
 		} else {
 			$this->stdResponse->success = FALSE;
@@ -97,23 +93,18 @@ class Admin_PrivilegeController extends App_Controller_Action {
 		$this->_helper->layout()->disableLayout();
 		$form = new Admin_Form_Privilege();
 
-		try {
-			$id = $this->_getParam('id', 0);
-			$privilegeMapper = new Model_PrivilegeMapper();
-			$privilege = $privilegeMapper->find($id);
-			if ($privilege != NULL) {//security
-				$form->getElement('name')->setValue($privilege->getName());
-				$form->getElement('description')->setValue($privilege->getDescription());
-				$form->getElement('module')->setValue($privilege->getModule());
-				$form->getElement('controller')->setValue($privilege->getController());
-				$form->getElement('action')->setValue($privilege->getAction());
-			} else {
-				$this->stdResponse->success = FALSE;
-				$this->stdResponse->message = _("The requested record was not found.");
-				$this->_helper->json($this->stdResponse);
-			}
-		} catch (Exception $e) {
-			$this->exception($this->view, $e);
+		$id = $this->_getParam('id', 0);
+		$privilegeMapper = new Model_PrivilegeMapper();
+		$privilege = $privilegeMapper->find($id);
+		if ($privilege != NULL) {//security
+			$form->getElement('name')->setValue($privilege->getName());
+			$form->getElement('description')->setValue($privilege->getDescription());
+			$form->getElement('module')->setValue($privilege->getModule());
+			$form->getElement('controller')->setValue($privilege->getController());
+			$form->getElement('action')->setValue($privilege->getAction());
+		} else {
+			$this->stdResponse->success = FALSE;
+			$this->stdResponse->message = _("The requested record was not found.");
 			$this->_helper->json($this->stdResponse);
 		}
 
@@ -136,35 +127,31 @@ class Admin_PrivilegeController extends App_Controller_Action {
 
 		$formData = $this->_request->getPost();
 		if ($form->isValid($formData)) {
-			try {
-				$id = $this->_getParam('id', 0);
+			$id = $this->_getParam('id', 0);
 
-				$privilegeMapper = new Model_PrivilegeMapper();
-				$privilege = $privilegeMapper->find($id);
-				if ($privilege != NULL) {
-					if (!$privilegeMapper->verifyExistName($formData['name']) || $privilegeMapper->verifyExistIdAndName($id, $formData['name'])) {
-						$privilege->setName($formData['name'])
-								->setDescription($formData['description'])
-								->setModule($formData['module'])
-								->setController($formData['controller'])
-								->setAction($formData['action'])
-								->setChangedBy(Zend_Auth::getInstance()->getIdentity()->id);
+			$privilegeMapper = new Model_PrivilegeMapper();
+			$privilege = $privilegeMapper->find($id);
+			if ($privilege != NULL) {
+				if (!$privilegeMapper->verifyExistName($formData['name']) || $privilegeMapper->verifyExistIdAndName($id, $formData['name'])) {
+					$privilege->setName($formData['name'])
+							->setDescription($formData['description'])
+							->setModule($formData['module'])
+							->setController($formData['controller'])
+							->setAction($formData['action'])
+							->setChangedBy(Zend_Auth::getInstance()->getIdentity()->id);
 
-						$privilegeMapper->update($id, $privilege);
+					$privilegeMapper->update($id, $privilege);
 
-						$this->stdResponse->success = TRUE;
-						$this->stdResponse->message = _("Privilege updated");
-					} else {
-						$this->stdResponse->success = FALSE;
-						$this->stdResponse->name_duplicate = TRUE;
-						$this->stdResponse->message = _("The Privilege already exists");
-					}
+					$this->stdResponse->success = TRUE;
+					$this->stdResponse->message = _("Privilege updated");
 				} else {
 					$this->stdResponse->success = FALSE;
-					$this->stdResponse->message = _("The Privilege does not exists");
+					$this->stdResponse->name_duplicate = TRUE;
+					$this->stdResponse->message = _("The Privilege already exists");
 				}
-			} catch (Exception $e) {
-				$this->exception($this->view, $e);
+			} else {
+				$this->stdResponse->success = FALSE;
+				$this->stdResponse->message = _("The Privilege does not exists");
 			}
 		} else {
 			$this->stdResponse->success = FALSE;
@@ -189,20 +176,16 @@ class Admin_PrivilegeController extends App_Controller_Action {
 
 		$itemIds = $this->_getParam('itemIds', array());
 		if (!empty($itemIds) ) {
-			try {
-				$privilegeMapper = new Model_PrivilegeMapper();
-				$removeCount = 0;
-				foreach ($itemIds as $id) {
-					$privilegeMapper->delete($id);
-					$removeCount++;
-				}
-				$message = sprintf(ngettext('%d privilege removed.', '%d privileges removed.', $removeCount), $removeCount);
-
-				$this->stdResponse->success = TRUE;
-				$this->stdResponse->message = _($message);
-			} catch (Exception $e) {
-				$this->exception($this->view, $e);
+			$privilegeMapper = new Model_PrivilegeMapper();
+			$removeCount = 0;
+			foreach ($itemIds as $id) {
+				$privilegeMapper->delete($id);
+				$removeCount++;
 			}
+			$message = sprintf(ngettext('%d privilege removed.', '%d privileges removed.', $removeCount), $removeCount);
+
+			$this->stdResponse->success = TRUE;
+			$this->stdResponse->message = _($message);
 		} else {
 			$this->stdResponse->success = FALSE;
 			$this->stdResponse->message = _("Data submitted is empty.");
@@ -287,24 +270,5 @@ class Admin_PrivilegeController extends App_Controller_Action {
 		}
 
 		return $filters;
-	}
-
-	/**
-	 *
-	 * Verifies if it exist some error with the application
-	 * @param Zend_View $view
-	 * @param Exception $e
-	 */
-	private function exception(Zend_View $view, Exception $e) {
-		$this->_logger->err($e->getMessage());
-		//response to client
-		$view->success = FALSE;
-		if ($e->getCode() == Model_EnumErrorType::APPLICATION)
-			$this->_messenger->addError($e->getMessage());
-		elseif ($e->getCode() == Model_EnumErrorType::DUP_NAME)
-			$this->_messenger->addError('name_warning');
-		else
-			$this->_messenger->addError(_("An error occurred while processing the data. <br/> Please Try again."));
-		$view->message = $view->seeMessages();
 	}
 }
